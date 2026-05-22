16a1f54fd32448864c63a3df2f3eecbc34109e7f
Jobo92800 <158471772+Jobo92800@users.noreply.github.com>
2026-05-22 19:13:20 +0200

    Add password protection component

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Layout from './components/Layout';
 import AppRoutes from './routes';
+import PasswordGate from './components/PasswordGate';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Layout>
-          <AppRoutes />
-        </Layout>
-      </Router>
-    </QueryClientProvider>
+    <PasswordGate>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Layout>
+            <AppRoutes />
+          </Layout>
+        </Router>
+      </QueryClientProvider>
+    </PasswordGate>
   );
 }
 
