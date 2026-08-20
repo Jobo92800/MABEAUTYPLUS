484b4c7d794b0b8dd4684346ba83b3661b330a7a
Jobo92800 <158471772+Jobo92800@users.noreply.github.com>
2026-08-20 11:55:04 +0200

    Remove application password gate

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,18 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Layout from './components/Layout';
 import AppRoutes from './routes';
-import PasswordGate from './components/PasswordGate';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <PasswordGate>
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <Layout>
-            <AppRoutes />
-          </Layout>
-        </Router>
-      </QueryClientProvider>
-    </PasswordGate>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Layout>
+          <AppRoutes />
+        </Layout>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
